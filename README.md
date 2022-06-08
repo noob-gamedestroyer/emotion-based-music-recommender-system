dc186d4903ce051c5a8e07ce6c6a136b0ef7b8d9
Rajat Kumar <60071765+noob-gamedestroyer@users.noreply.github.com>
2022-06-09 02:16:59 +0530

    Lots of changes

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-To run this project add client id, secret and username in 
-helpers.py and Keras-Classification.ipynb
+To run this project add `client_id`, `secret` and `username` in 
+`application.properties` file.
+ 
+* Username can be retrieved from account section in spotify app.
 
-Username can be retrieved from account section in spotify app
+* also add the redirect-uri to developer console in edit settings option.
 
-also add the redirect-uri to developer console in edit settings option
+* and don't forget to add `application.properties` file to project root.
+
+#### Format for `application.properties` file: 
+
+* `[SpotifyCredentials]`
+* `client_id = your_client_id`
+* `client_secret = your_client_secret`
+* `username = your_username`
+* `redirect_uri = http://localhost:8080/callback`
--- helpers.py
+++ helpers.py
@@ -1,21 +1,24 @@
 import spotipy
 import time
+import configparser
 from IPython.display import clear_output
 from spotipy import SpotifyClientCredentials, util
 
+config = configparser.ConfigParser()
+config.read('application.properties')
 
-client_id='your client id'
-client_secret='your client secret'
-redirect_uri='http://localhost:8080/callback'
-username = 'your username'
+client_id = config.get("SpotifyCredentials", "client_id")
+client_secret = config.get("SpotifyCredentials", "client_secret")
+redirect_uri = config.get("SpotifyCredentials", "redirect_uri")
+username = config.get("SpotifyCredentials", "username")
 scope = 'playlist-modify-public'
 
-#Credentials to access the Spotify Music Data
-manager = SpotifyClientCredentials(client_id,client_secret)
+# Credentials to access the Spotify Music Data
+manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=manager)
 
-#Credentials to access to  the Spotify User's Playlist, Favorite Songs, etc.
-token = util.prompt_for_user_token(username,scope,client_id,client_secret,redirect_uri)
+# Credentials to access to  the Spotify User's Playlist, Favorite Songs, etc.
+token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
 spt = spotipy.Spotify(auth=token)
 
 
@@ -26,15 +29,16 @@ def get_albums_id(ids):
         album_ids.append(album['id'])
     return album_ids
 
+
 def get_album_songs_id(ids):
     song_ids = []
-    results = sp.album_tracks(ids,offset=0)
+    results = sp.album_tracks(ids, offset=0)
     for songs in results['items']:
         song_ids.append(songs['id'])
     return song_ids
 
-def get_songs_features(ids):
 
+def get_songs_features(ids):
     meta = sp.track(ids)
     features = sp.audio_features(ids)
 
@@ -45,7 +49,7 @@ def get_songs_features(ids):
     release_date = meta['album']['release_date']
     length = meta['duration_ms']
     popularity = meta['popularity']
-    ids =  meta['id']
+    ids = meta['id']
 
     # features
     acousticness = features[0]['acousticness']
@@ -61,10 +65,12 @@ def get_songs_features(ids):
     time_signature = features[0]['time_signature']
 
     track = [name, album, artist, ids, release_date, popularity, length, danceability, acousticness,
-            energy, instrumentalness, liveness, valence, loudness, speechiness, tempo, key, time_signature]
-    columns = ['name','album','artist','id','release_date','popularity','length','danceability','acousticness','energy','instrumentalness',
-                'liveness','valence','loudness','speechiness','tempo','key','time_signature']
-    return track,columns
+             energy, instrumentalness, liveness, valence, loudness, speechiness, tempo, key, time_signature]
+    columns = ['name', 'album', 'artist', 'id', 'release_date', 'popularity', 'length', 'danceability', 'acousticness',
+               'energy', 'instrumentalness',
+               'liveness', 'valence', 'loudness', 'speechiness', 'tempo', 'key', 'time_signature']
+    return track, columns
+
 
 def get_songs_artist_ids_playlist(ids):
     playlist = sp.playlist_tracks(ids)
@@ -74,10 +80,10 @@ def get_songs_artist_ids_playlist(ids):
         songs_id.append(result['track']['id'])
         for artist in result['track']['artists']:
             artists_id.append(artist['id'])
-    return songs_id,artists_id
+    return songs_id, artists_id
 
-def download_albums(music_id,artist=False):
 
+def download_albums(music_id, artist=False):
     if artist == True:
         ids_album = get_albums_id(music_id)
     else:
@@ -88,9 +94,9 @@ def download_albums(music_id,artist=False):
 
     tracks = []
     for ids in ids_album:
-        #Obtener Ids de canciones en album
+        # Obtain Ids contained in album
         song_ids = get_album_songs_id(ids=ids)
-        #Obtener feautres de canciones en album
+        # Obtain features contained in album
         ids2 = song_ids
 
         print(f"Album Length: {len(song_ids)}")
@@ -100,18 +106,19 @@ def download_albums(music_id,artist=False):
         tracks.append(track)
 
         print(f"Song Added: {track[0]} By {track[2]} from the album {track[1]}")
-        clear_output(wait = True)
+        clear_output(wait=True)
 
-    clear_output(wait = True)
+    clear_output(wait=True)
     print("Music Downloaded!")
 
-    return tracks,columns
+    return tracks, columns
 
-def download_playlist(id_playlist,n_songs):
+
+def download_playlist(id_playlist, n_songs):
     songs_id = []
     tracks = []
 
-    for i in range(0,n_songs,100):
+    for i in range(0, n_songs, 100):
         playlist = spt.playlist_items(id_playlist, limit=100, offset=i)
 
         for songs in playlist['items']:
@@ -119,24 +126,51 @@ def download_playlist(id_playlist,n_songs):
 
     counter = 1
     for ids in songs_id:
-
         time.sleep(.6)
-        track,columns = get_songs_features(ids)
+        track, columns = get_songs_features(ids)
         tracks.append(track)
 
         print(f"Song {counter} Added:")
         print(f"{track[0]} By {track[2]} from the album {track[1]}")
-        clear_output(wait = True)
-        counter+=1
+        clear_output(wait=True)
+        counter += 1
 
-    clear_output(wait = True)
+    clear_output(wait=True)
     print("Music Downloaded!")
 
-    return tracks,columns
+    return tracks, columns
 
 
-# def spotify_recommend():
-#     seed_tra = ['7Cq27Qc3WRyQ3ddwVslpVZ']
-#     rec = sp.recommendations(seed_artists=[], seed_tracks=seed_tra, seed_genres=[], limit=10)
-#     id_list = [rec['tracks'][i]['id'] for i in range(len(rec['tracks']))]
-#     return id_list
+def recommend_default_emotion_songs(str_emotion):
+    seed_tra_dic = {
+        'Happy': [
+            '27NovPIUIRrOZoCHxABJwK',  # industry baby - lil nas
+            '2CEgGE6aESpnmtfiZwYlbV',  # dynamite - taio
+            '0HPD5WQqrq7wPWR7P7Dw1i',  # tik tok - kesha
+            '2LEF1A8DOZ9wRYikWgVlZ8',  # good feeling - flo rida
+            '0ct6r3EGTcMLPtrXHDvVjc',  # the nights - avicii
+        ],
+        'Sad': [
+            '3S8jK1mGzQi24ilFb45DAZ',  # lonely - JB
+            '7smPGXPTC7rfvvYBs624hl',  # home - mike posner
+            '2TIlqbIneP0ZY1O0EzYLlc',  # someone you loved - lewis
+            '7ekZl2me2Fjn6ywN16cvFK',  # memories
+            '779UN3kabApm2zfqX549vf',  # when was it over
+        ],
+        'Energetic': [
+            '3EtyEzMpfKSaoVhPunvbRV',  # leave me alone - NF
+            '7Cq27Qc3WRyQ3ddwVslpVZ',  # heart attack - scarlord
+            '6LNoArVBBVZzUTUiAX2aKO',  # off the grid - kanye
+            '7hdw5gAGMyyn9z0XgfTv1j',  # no name - nf
+        ],
+        'Calm': []
+    }
+    return seed_tra_dic[str_emotion]
+
+
+def spotify_recommend():
+    emotion_str = 'Happy'  # TODO: get a emotion as string from Emotion recognition model
+    seed_tra = recommend_default_emotion_songs(emotion_str)
+    rec = sp.recommendations(seed_tracks=seed_tra, limit=10)
+    id_list = [rec['tracks'][i]['id'] for i in range(len(rec['tracks']))]
+    return id_list
